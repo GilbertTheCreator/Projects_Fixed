ac22a377ab0e96c9e7a2cc8d9b1fa6a5b20b438e
Matthew <airless@gmail.com>
2023-12-19 19:41:27 -0500

    Update

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
-import express from 'express';
-import { MongoClient } from 'mongodb';
+const express = require('express');
+const mongoose = require('mongoose');
+const { body, validationResult } = require('express-validator');
 
 const port = 3000;
 const app = express();
-//connect to database
-const uri = "mongodb+srv://Gilbert:V89EKOUwRu3x41sO@1.h0mvxrz.mongodb.net/users";
+app.use(express.json());
 
-let db;
+mongoose.connect('mongodb+srv://Gilbert:V89EKOUwRu3x41sO@1.h0mvxrz.mongodb.net/users', { useNewUrlParser: true, useUnifiedTopology: true });
 
-(async function () {
-    try {
-        const client = await MongoClient.connect(uri, { useUnifiedTopology: true });
-        //log in console that you have connected to MongoDB
-        console.log('Connected to MongoDB.');
-        
-        db = client.db("users");
-        //Creates a new collection in the db called users
-        await db.createCollection("Users");
-        //Add a username named Jay age 25 to the users database
-        await db.collection('Users').insertOne({ name: "Jay", age: 25 });
-        await db.collection('Users').insertOne({ name: "Xavier", age: 21 });
+//Creates a user schema
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
 
-    } catch (err) {
-        console.error('Error occurred while connecting to MongoDB:', err);
-    }
-})();
+//Declares userschema as user
+const User = mongoose.model('Users', userSchema);
 
-app.get('/', async (req, res) => {
-    try {
-        //Gets users in database
-        const users = await db.collection('Users').find().toArray();
 
-        res.send(users);
-    } catch (err) {
-        console.error('Error', err);
+//Get users
+app.get('/Users', async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+//Displays users age and name in the database
+app.post('/Users', [
+    body('name').isString(),
+    body('age').isInt()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
 });
 
